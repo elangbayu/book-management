d1f30ead9ba05f532a5bdb3dd122e6fb78c41174
elangbayu <elangbayu17@gmail.com>
2023-03-15 19:43:03 +0700

    add menu to separate class

---
+++ src/main/java/org/alterra/utils/Menu.java
@@ -0,0 +1,25 @@
+package org.alterra.utils;
+
+public class Menu {
+    public static void showMenu() {
+        System.out.println("Welcome to book management app");
+        System.out.println("Please choose your menu:");
+        System.out.println("1. Create a new book");
+        System.out.println("2. Get all books");
+        System.out.println("3. Get book by ID");
+        System.out.println("4. Update book");
+        System.out.println("5. Delete book");
+        System.out.println("6. Exit");
+    }
+
+    public static void showMenuIndo() {
+        System.out.println("Selamat datang di Aplikasi Manajemen Buku");
+        System.out.println("Silakan pilih menu:");
+        System.out.println("1. Buat buku baru");
+        System.out.println("2. List semua buku");
+        System.out.println("3. Detail buku dari ID");
+        System.out.println("4. Ubah buku");
+        System.out.println("5. Hapus buku");
+        System.out.println("6. Keluar");
+    }
+}
